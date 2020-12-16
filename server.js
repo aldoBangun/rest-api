9ca0a5323fd6ed06b88482b6ca1071d699bb299b
aldoBangun <punk.aldo68@gmail.com>
2020-12-17 03:14:40 +0800

    add users router to server.js

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const itemsRouter = require("./routes/items.route");
+const usersRouter = require("./routes/users.route");
 
 dotenv.config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/items", cors(), itemsRouter);
+app.use("/users", cors(), usersRouter);
 
 const PORT = process.env.APP_PORT || 5000;
 app.listen(PORT, console.log(`Server is running on http://localhost:${PORT}`));
