8bd712a190e240da19ab379b35425c8cabcb9c45
aldoBangun <punk.aldo68@gmail.com>
2020-12-16 11:32:07 +0800

    add body-parser middleware to server.js

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 const itemsRouter = require("./routes/items.route");
 
 dotenv.config();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/items", cors(), itemsRouter);
 
 const PORT = process.env.APP_PORT || 5000;
