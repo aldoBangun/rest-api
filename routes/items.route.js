97bb2df2c227bac558f11fcb2876c2dc7537aa70
aldoBangun <punk.aldo68@gmail.com>
2020-12-16 11:31:07 +0800

    add items route

--- routes/items.route.js
+++ routes/items.route.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getItems } = require("../controllers/items.controller");
+const {
+   getItems,
+   getItem,
+   deleteItems,
+   deleteItem,
+   addItem,
+   updateItem,
+} = require("../controllers/items.controller");
 
-router.route("/").get(getItems);
+router.route("/").get(getItems).post(addItem).delete(deleteItems);
+
+router.route("/:id").get(getItem).patch(updateItem).delete(deleteItem);
 
 module.exports = router;
